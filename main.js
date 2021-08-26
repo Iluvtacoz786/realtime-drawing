b72b56236f5a4d6b05fb559ceaea9e5cf050d70a
Zoya Rana <74219818+Iluvtacoz786@users.noreply.github.com>
2021-08-25 17:50:03 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function preload(){}
+function setup(){
+    canvas=createCanvas(500,500)
+    video=createCapture(VIDEO)
+    canvas.position(750,170)
+    video.size(500,500)
+}
+function draw(){}
